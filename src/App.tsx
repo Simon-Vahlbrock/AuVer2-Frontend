4748b6d5e21933329d8aeb08aca2ab2d91acf84a
Simon <simon.vahlbrock@tobit.com>
2023-11-06 14:47:21 +0100

    ✨ Config vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+const App: FC = () => {
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+        </div>
+    );
+};
+
+App.displayName = 'App';
+
+export default App;
